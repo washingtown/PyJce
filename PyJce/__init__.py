8d9044329e3c792baf4a968486b176ae803088be
washingtown <sharp-shoot2@126.com>
2020-03-23 09:38:43 +0800

    调整目录结构，增加pip打包

--- PyJce/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bytebuffer import ByteBuffer
-from .struct import JceStruct
-from .stream import JceInputStream
-
-name = "PyJce"
--- PyJce/exception.py
+++
@@ -1,7 +0,0 @@
-# class ByteBufferExcetion(ValueError):
-#     def __init__(self,ErrorInfo):
-#         super().__init__(self)
-#         self.errorinfo = ErrorInfo
-
-class JceDecodeException(ValueError):
-    pass
